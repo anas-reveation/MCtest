c7618d2049bad91f68ad1d9fe2f509d51e39098e
anas <anas.khan@reveation.io>
2024-01-19 19:41:47 +0530

    Add: remove the async code

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,11 @@
 <template>
   <div>
     <h1>hellow world</h1>
-    <div v-if="this.posts?.data?.message">
-      {{ this.posts.data.message }}
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      posts: [],
-      message: 'Loading...', // Set a default value
-    };
-  },
-  async fetch() {
-    this.posts = await this.$axios.get('/api');
-    console.log('post data reponce', this.posts);
-  },
+  data() {},
 };
 </script>
